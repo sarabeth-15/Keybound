876e56943a41d113fee4f97a7badf0dc24b0a83d
Sara P <148921997+sarabeth15@users.noreply.github.com>
2025-02-21 16:48:11 -0500

    fixed player movement

--- block-digital-prototype-1/Assets/Player Character/PlayerMovement.cs
+++ block-digital-prototype-1/Assets/Player Character/PlayerMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovements : MonoBehaviour {
 
     public float speed;
 
--- block-digital-prototype-1/Assets/Scripts/PlayerMovement.cs
+++ block-digital-prototype-1/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript : MonoBehaviour 
+public class PlayerMovement : MonoBehaviour 
 {
-    
+    private Rigidbody2D body;
+
+    private void Awake() {
+        body = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update() {
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal"), body.linearVelocity.y); 
+    }
 }
