4ba3d1f48f2eaffb8223e333aa0b11edb87431fc
saph-i <116207834+saph-i@users.noreply.github.com>
2025-02-22 18:50:21 -0500

    updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     private bool grounded;
+    private float jumpForce = 10; 
+    private bool jumpPressed = false;
     
     [SerializeField] private float speed;
 
@@ -18,16 +20,22 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update() 
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            horizontalInput = -1f;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            horizontalInput = 1f;
+
         body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
         
         //flip player left or right
         if(horizontalInput>0.01f && Input.GetKey(KeyCode.RightArrow))
-            transform.localScale = new Vector3(1,2,1);
+            transform.localScale = new Vector3(1,1,1);
         else if(horizontalInput<-0.01f&& Input.GetKey(KeyCode.LeftArrow))
-            transform.localScale = new Vector3(1,2,1);
+            transform.localScale = new Vector3(1,1,1);
         
-        if(Input.GetKey(KeyCode.UpArrow) && grounded)
+        if(Input.GetKeyDown(KeyCode.UpArrow) && grounded && !jumpPressed)
             Jump();
 
         //animation parameters
@@ -36,14 +44,22 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Jump()
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x, 6);
+        body.linearVelocity = new Vector2(body.linearVelocity.x, 3);
+        body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         anim.SetTrigger("jump");
         grounded = false;
+        jumpPressed = true;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag =="Ground")
+        {
             grounded = true;
+            //Prevent vertical bouncing after landing
+            if (body.linearVelocity.y < 0) 
+                body.linearVelocity = new Vector2(body.linearVelocity.x, 0);
+            jumpPressed = false;
+        }
     }
 
 }
