f813d7ac7e62b64564849b06497ead839f5aab88
Sara P <148921997+sarabeth-15@users.noreply.github.com>
2025-03-16 13:40:50 -0400

    Fixed options menu

--- Assets/Scripts/ScreenScripts/MainMenu.cs
+++ Assets/Scripts/ScreenScripts/MainMenu.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement; 
 
 public class MainMenu : MonoBehaviour {
+
+    [SerializeField] GameObject optionsMenu;
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.S)) {
             SceneManager.LoadScene("Level0"); 
         }
 
         if (Input.GetKeyDown(KeyCode.O)) {
-            SceneManager.LoadScene("Options"); 
+            optionsMenu.SetActive(true);    
         }
 
         if (Input.GetKeyDown(KeyCode.Q)) {
