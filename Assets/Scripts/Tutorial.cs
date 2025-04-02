17e74ff8c95b35f4f264f44aa1e73d8c95f5e1fe
saph-i <116207834+saph-i@users.noreply.github.com>
2025-04-01 23:33:59 -0400

    tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -3,40 +3,55 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     public GameObject[] popUps;
-    private int popUpIndex;
+    private int popUpIndex = 0;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        for(int i=0; i<popUps.Length;i++){
-            if(i==popUpIndex){
-                popUps[popUpIndex].SetActive(true);
-            }else{
-                popUps[popUpIndex].SetActive(false);
+        // Update which tutorial pop-up is active
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if(i== popUpIndex){
+                popUps[i].SetActive(true);
+            }
+            else{
+                popUps[i].SetActive(false);
+            
+        }
+        }
+        if (popUpIndex ==0)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow)||Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                popUpIndex++; // Move to next tutorial step
             }
         }
-        if(popUpIndex == 0){
-            foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode))){
-                if (Input.GetKeyDown(key) && ((key >= KeyCode.A && key <= KeyCode.Z) || (key >= KeyCode.Alpha0 && key <= KeyCode.Alpha9)))
-                {
-                    popUpIndex++; //first press of the bricks allows the player to move to next tutorial
-                }
-            }
-            if(popUpIndex == 1){
-                if(Input.GetKeyDown(KeyCode.KeypadEnter)){
-                    popUpIndex++;
-                }
-            }
-            else if(popUpIndex == 1){
-                if(Input.GetKeyDown(KeyCode.KeypadEnter)){
-                    popUpIndex++;
-                }
+        else if (popUpIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                popUpIndex++; // Move to next tutorial step
             }
         }
+
+        // First tutorial step: Check for specific key presses (z, x, c, v, b, n, m)
+        else if (popUpIndex == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) ||
+                Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.V) ||
+                Input.GetKeyDown(KeyCode.B) || Input.GetKeyDown(KeyCode.N) ||
+                Input.GetKeyDown(KeyCode.M))
+            {
+                popUpIndex++; // Move to next tutorial step
+            }
+        }
+        // Second tutorial step: Wait for KeypadEnter
+        else if (popUpIndex == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                popUpIndex++;
+            }
+        }
+        // Third tutorial step: Wait for KeypadEnter again
     }
 }
